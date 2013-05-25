479a9a008fc05b1f5d93c3d8d73a62fccc86b08c
Seth Vincent <sethvincent@gmail.com>
2013-05-25 15:51:40 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-$.getJSON('http://data.seattle.gov/api/views/3k2p-39jp/rows.json?jsonp=?&max_rows=25', function(results) {
+// http://bit.ly/177Mb8J
+
+var dataUrl = 'http://data.seattle.gov/api/views/3k2p-39jp/rows.json?jsonp=?&max_rows=25';
+
+$.getJSON(dataUrl, function(results) {
   console.log(results.data);
 
   var map = L.map('map').setView([47.6097, -122.3331], 11);
 
   L.tileLayer('http://{s}.tile.cloudmade.com/5aabd2e38b214356942b011b42b77394/997/256/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+      attribution: 'attribution'
   }).addTo(map);
 
   $.each(results.data, function(index, value) {
-    var date = new Date(value[3]*1000);
-    var moment_date = moment(date).format("h:mm a, MMM D, YYYY")
 
     L.marker([value[21], value[20]]).addTo(map)
-    .bindPopup('<h6>' + value[12] + '</h6>' + value[16] + '<br>' + moment_date );
+    .bindPopup('<h6>' + value[12] + '</h6>' + value[16] + '<br>' + value[3] );
   });
 
 });
-
-
